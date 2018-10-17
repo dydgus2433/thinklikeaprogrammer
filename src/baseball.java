9d83e53910a8df5785e62c42ce68d7ca00233029
AJ-02 <AJ-02@AJ-02-PC>
2018-10-17 21:44:05 +0900

    tetris ing

--- src/baseball.java
+++ src/baseball.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class baseball {
@@ -7,92 +8,92 @@ public class baseball {
 
 		Scanner sc = new Scanner(System.in);
 		int answerCnt = sc.nextInt();
+				
+		ArrayList<Integer> numArr = new ArrayList<>();
+		ArrayList<Integer> inputArr = new ArrayList<>();
 		
-		//서로 다른 세 숫자를 마음속으로 생각한다 ( 랜덤수) 각각 다름
+		ArrayList<Integer> strikeArr = new ArrayList<>();
+		ArrayList<Integer> ballArr = new ArrayList<>();
 		
-		int a = 324;
+		HashSet<Integer> answerArr = new HashSet<>();
 		
-		//다른 세자리수를 묻는다
-		int b = 123;
-		//스트라이크 볼로 대답한다.
-		int strike = 1;
-		int ball = 1;
-		//3스트라이크면 게임이 끝난다.
-		if (strike == 3 ) {
-			System.out.println("end");
+		for(int i = 0; i < answerCnt; i++) {
+			inputArr.add( sc.nextInt() );
+			strikeArr.add( sc.nextInt() );
+			ballArr.add( sc.nextInt() );
 		}
-		//아니라면 다시 묻는다
-		else {
+		
+		
+		
+		for(int ii = 123; ii <=987; ii++) {			
+			int first = ii /100;
+			int second = (ii % 100) /10;
+			int third = (ii % 100) %10;			
+			if((first != second)  && (second != third) && (first != third)&& (first != 0 && second != 0 && third != 0)) {
+				numArr.add(ii);
+			}						
+		}
+		
+		
+		for(int iii =0; iii < numArr.size(); iii++) {
+			
+			
+			int first = numArr.get(iii) /100;
+			int second = (numArr.get(iii) % 100) /10;
+			int third = (numArr.get(iii) % 100) %10;	
+			
+			
+			for(int j =0; j < inputArr.size(); j++) {
+				int strikeCnt = 0;
+				int ballCnt = 0;
+				int inFirst = inputArr.get(j) /100;
+				int inSecond = (inputArr.get(j) % 100) /10;
+				int inThird = (inputArr.get(j) % 100) %10;	
+				
+				if(first == inFirst ) {
+					strikeCnt++;
+				}else{
+					if(second == inFirst) {
+						ballCnt++;
+					}else if(third == inFirst) {
+						ballCnt++;
+					}
+					
+				}
+				
+				if(second == inSecond ) {
+					strikeCnt++;
+				}else {
+					if(first == inSecond) {
+						ballCnt++;
+					}else if(third == inSecond) {
+						ballCnt++;
+					}
+				}
+				
+				if(third == inThird ) {
+					strikeCnt++;
+				}else {
+					if(first == inThird) {
+						ballCnt++;
+					}else if(second == inThird) {
+						ballCnt++;
+					}
+				}
+				if(strikeArr.get(j) == strikeCnt && ballArr.get(j) == ballCnt) {
+					if(j == inputArr.size()-1) {
+						answerArr.add(numArr.get(iii));
+					}					
+				}else {
+					break;
+				}
+				
+				
+			}
+			
+		}
+		
+		System.out.println(answerArr.size());
 	
-			System.out.println("continue");
-		}
-		
-		//영수가 생각하고 있을 가능성이 있는 수가 총 몇 개인지를 알아맞혀야 한다.
-		 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-//		ArrayList<Integer> numArr = new ArrayList<>();
-//		ArrayList<Integer> strikeArr = new ArrayList<>();
-//		ArrayList<Integer> ballArr = new ArrayList<>();
-//		for(int ii = 1; ii < 10; ii++) {
-//			numArr.add(ii);
-//		}
-//		System.out.println(numArr.toString());
-//		String question = "";
-//		for(int i =0 ; i < answerCnt; i++) {
-//			question = sc.next();		
-//			
-//			int strike = sc.nextInt();
-//			int ball = sc.nextInt();
-//			
-//			if(strike == 0 && ball == 0) {
-//				for(int jj =0; jj < 3; jj++) {
-//					for(int j =0; j < numArr.size(); j++) {
-//					
-//					 
-////						System.out.println(question.charAt(jj));
-//						if(Integer.parseInt(question.charAt(jj)+"") == numArr.get(j)) {
-//							numArr.remove(j);
-////							System.out.println(numArr.toString());
-//							break;
-//						}
-//					}
-//				}
-////				numArr.remove();
-//			}else if(strike == 1 && ball == 0) {
-//				
-//				//
-//				
-//				
-//				
-//			}else if(strike == 1 && ball == 1) {
-//				
-//			}else if(strike == 1 && ball == 2) {
-//				
-//			}else if(strike == 2 && ball == 0) {
-//				
-//			}else if(strike == 3 && ball == 0) {
-//				
-//			}
-//			
-//			
-//		}	
-//		System.out.println(numArr.toString());
-//		
-//		int result = 1;
-//		for(int jj =0; jj < 3; jj++) {				
-//			result *= (numArr.size() - jj);
-//		}
-//		System.out.println(result);
-//	
 	}
 }
