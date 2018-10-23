201d354efa268f0f1da58151c4a5cc433a37ed7b
LeeYH <dydgus2433@naver.com>
2018-10-23 16:29:10 +0900

    최소공배수 최대공약수

--- src/gcdlcm.java
+++ src/gcdlcm.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class gcdlcm {
@@ -15,33 +17,53 @@ public class gcdlcm {
 		int gcd =0;
 		int lcm = 1;
 		
-		for(int i =2; i < num1; i++) {
+		for(int i =1; i <= num1; i++) {
 			if(num1 % i ==0) {
 				arr1.add(i);
 			}
 		}
 		
-		for(int i =2; i < num2; i++) {
+		for(int i =1; i <= num2; i++) {
 			if(num2 % i ==0) {
 				arr2.add(i);
 			}
 		}
-		System.out.println(arr1.toString());
+		//System.out.println(arr1.toString());
 		for(int i =0; i < arr1.size(); i++) {
 			for(int j =0; j < arr2.size(); j++) {
 				if(arr1.get(i) == arr2.get(j)) {
-					gcd = arr2.get(j);
-					
-				}else {
-					arr3.add(arr2.get(j));
+					gcd = arr2.get(j);					
 				}
 			}
 		}
-		System.out.println(arr1.toString());
+
 		System.out.println(gcd);
 		
-		for(int i =0; i < arr1.size(); i++) {
-			lcm *= arr1.get(i);
+		int temp1 = num1; 
+		int temp2 = num2;
+		int cnt = 2;
+		while(true) {
+			
+			if(temp1 % cnt == 0 && temp2 % cnt == 0) {
+				arr3.add(cnt);
+				temp1 /= cnt;
+				temp2 /= cnt;
+				continue;
+			}else {
+				
+				if((temp1 % cnt != 0 || temp2 % cnt != 0) && (cnt > temp1  || cnt > temp2) ) {
+					arr3.add(temp1);
+					arr3.add(temp2);
+					break;
+				}else {
+					cnt++;
+				}
+			}
+		}
+		//System.out.println(arr3.toString());
+		
+		for(int i = 0 ; i < arr3.size(); i++) {
+			lcm *= arr3.get(i);
 		}
 		System.out.println(lcm);
 	}
