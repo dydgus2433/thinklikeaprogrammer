f4a58f816b3e1f6aa3a6209458eef78ade3ab727
AJ-03 <AJ-03@AJ-03-PC>
2018-10-28 13:04:05 +0900

    알고리즘 강의 주소

--- src/버블정렬.java
+++ src/버블정렬.java
@@ -2,6 +2,8 @@
 public class 버블정렬 {
 
 	public static void main(String[] args) {
+		//https://vimeo.com/album/5266174
+		//https://swexpertacademy.com/main/capacityTest/main.do
 		int[] arr = {23,4,5,2,3,6,7,11,323,1230,3123,123,4512,34123,512,3123123,213,112};
 		
 		for(int i =0; i < arr.length; i++) {
